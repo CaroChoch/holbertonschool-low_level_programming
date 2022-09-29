db64921c20a562c7d0abbe04d1eb9d54901aaa10
CaroChoch <5692@holbertonstudents.com>
2022-09-29 14:47:43 -0700

    puts function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 /**
- *"main" function : prints out a string to stdout.
- * "puts" function in C is used to write a line or string
+ * description : puts function in C is used to write a line or string
  * to the output (stdout) stream.
  * it prints the passed string with a new line and returns
  * an integer value.
  * The ruturn value depends on the success of the wrinting procedure
  */
 int main(void)
+/**
+ * description : main is un function who prints out a string to stdout
+ */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
