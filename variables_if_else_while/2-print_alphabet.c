ec58f95a21b8a51f90bba8b2743a1af972e36cc2
CaroChoch <5692@holbertonstudents.com>
2022-10-03 05:55:37 -0700

    putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,19 @@
-/*
- * File: 2-print_alphabet.c
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
 	char letter;
 
-	for (letter = "a"; letter <= "z"; letter ++);
-	putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-        putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
+
