026dfd1c4601dcc7968910ec6d4ad55f789a0b78
CaroChoch <5692@holbertonstudents.com>
2022-10-10 03:23:04 -0700

    task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 /**
  * main - function that takes a pointer to an int as parameter and
  * update the value it points to to 98.
- * reset_to_98: reset the value of pointer to 98.
+ * reset_to_98 - resets the value of pointer to 98.
  * @n:pointer to reset to 98.
  * Return: void.
  */
