2987889c8b6c6bdb6e96aff1c10ba7b3e34c80d9
CaroChoch <5692@holbertonstudents.com>
2022-10-12 03:40:28 -0700

    function that capitalizes all words of a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,24 +15,22 @@ char *cap_string(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-
-		if (str[i - 1] == ' ' ||
-		    str[i - 1] == '\t' ||
-		    str[i - 1] == '\n' ||
-		    str[i - 1] == ',' ||
-		    str[i - 1] == ';' ||
-		    str[i - 1] == '.' ||
-		    str[i - 1] == '!' ||
-		    str[i - 1] == '?' ||
-		    str[i - 1] == '"' ||
-		    str[i - 1] == '(' ||
-		    str[i - 1] == ')' ||
-		    str[i - 1] == '{' ||
-		    str[i - 1] == '}' ||
-		    i == 0)
-			str[i] -= 32;
-
-		i++;
+		{
+			if (i == 0)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] >= 9 && str[i - 1] <= 10)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] >= 32 && str[i - 1] <= 34)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] >= 40 && str[i - 1] <= 41)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] == 46)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] == 59)
+				str[i] = str[i] - 32;
+			else if (str[i - 1] == 123 || str[i - 1] == 125)
+				str[i] = str[i] - 32;
+		}
 	}
 
 	return (str);
