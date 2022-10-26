cd857a395a2b048caf6c7b686c0751fc7b1c13a1
CaroChoch <5692@holbertonstudents.com>
2022-10-26 06:38:07 -0700

    program that print the name of the file it was compiled from

---
+++ preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#incmude <stdlib.h>
+
+/**
+ * main - prints the name of the file
+ * compiled from.
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
