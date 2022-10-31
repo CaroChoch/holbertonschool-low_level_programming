05b16659fb728be132ef0c29fa669db21e373469
CaroChoch <5692@holbertonstudents.com>
2022-10-31 09:30:18 -0700

    function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -76,11 +76,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog_name);
 		return (NULL);
 	}
-
 	new_dog_name->name = _strcpy(new_dog_name->name, name);
 	new_dog_name->age = age;
 	new_dog_name->owner = _strcpy(new_dog_name->owner, owner);
-
 	return (new_dog_name);
 }
 
