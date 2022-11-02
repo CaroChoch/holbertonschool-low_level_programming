5d4332d7920766789cb0f399c8e8027037648703
CaroChoch <5692@holbertonstudents.com>
2022-11-02 03:34:10 -0700

    update betty coding style

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -17,14 +17,14 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-        op_t ops[] = {
-                {"+", op_add},
-                {"-", op_sub},
-                {"*", op_mul},
-                {"/", op_div},
-                {"%", op_mod},
-                {NULL, NULL}
-        };
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
 	int i = 0;
 
@@ -36,6 +36,13 @@ int (*get_op_func(char *s))(int, int)
 	}
 	return (NULL);
 }
+
+
+
+
+
+
+
 
 
 
