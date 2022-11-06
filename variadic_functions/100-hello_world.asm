2b952ce1a45462b71a8155341ed259a9202fa4f0
CaroChoch <5692@holbertonstudents.com>
2022-11-06 13:26:49 -0800

    a 64-bit program in assembly that prints Hello, World, followed by a new line

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -2,14 +2,15 @@ section .text
 global main
 
 main:
-        mov rdx, msglen
-        mov rsi, msg
-        mov rax, 1
-        mov rdi, 1
+        mov rdx, msglen; sizeof the string
+        mov rsi, msg; string
+        mov rax, 1; write
+        mov rdi, 1; stout
         syscall
 
-        mov rax, 60
-        syscall
+        mov rax, 60; exit
+	mov rdi, 0; exit success 
+	syscall
 
 section .data
 	msg: db "Hello, World", 10
