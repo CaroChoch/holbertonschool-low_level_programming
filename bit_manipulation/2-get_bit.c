f0cba295c7d59e0b00cb632f6e597179a6ad9270
CaroChoch <5692@holbertonstudents.com>
2022-11-18 07:16:26 -0800

    betty style code

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -16,8 +16,8 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bit = (n >> index) & 1;
 
 	if (index >= sizeof(unsigned int) * 8)
-                return (-1);
-	
+		return (-1);
+
 	return (bit);
 }
 
