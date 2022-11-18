f8e973d1058c0908cc95bcf1e41dc945acc02504
CaroChoch <5692@holbertonstudents.com>
2022-11-18 08:01:39 -0800

    add a size variable

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * binary_to_uint - function that
@@ -17,16 +18,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int val = 0;
-	int i = 0;
+	int i, size = strlen(b);
 
 	if (b == NULL)
-	return 0;
+		return (0);
 
-	for (i = 0; b[i] == '0' || b[i] == '1'; i++)
+	for (i = 0; i < size; i++)
 	{
+		if (b[i] == '0' || b[i] == '1')
+		{
 		val <<= 1;
-		val += b[i]-'0';
-	}
+		val += b[i] - '0';
+		}
+		else
+			return (0);
+	}	
+
 	return (val);
 }
 
