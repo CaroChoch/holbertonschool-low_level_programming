32688087ed15f1b514c2454993cc2b6e5940ec01
CaroChoch <5692@holbertonstudents.com>
2022-11-21 06:40:15 -0800

    condition changes

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	file_from = open(argv[1], O_RDONLY);
-	if (!file_from)
+	if (file_from < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
@@ -26,16 +26,16 @@ int main(int argc, char *argv[])
 	while ((n = read(file_from, buffer, 1024)) > 0)
 		if ((write(file_to, buffer, n)) != n || file_to < 0)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	if (!n)
+	if (n < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	
+
 	if (close(file_from))
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
 	if (close(file_to))
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
-	
+
 	return (0);
 }
