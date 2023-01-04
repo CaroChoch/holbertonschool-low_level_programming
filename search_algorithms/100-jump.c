e4bade49fabea471fa73dfe57678db5f1a4aedf8
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-04 19:21:24 +0100

    Betty style code

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -3,11 +3,11 @@
 /**
  * jump_search - function that searches for a value in a sorted array of
  * integers using the Jump search algorithm.
- * 
+ *
  * @array: pointer to the first element of the array to search in.
  * @size:  the number of elements in array.
  * @value: the value to search for.
- * 
+ *
  * Return: the first index where value is located or if value is not present
  * in array or if array is NULL, your function must return -1.
  */
