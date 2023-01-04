31da6a52ca771962ac2642c113c3e64656ec0e32
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-01-04 23:20:15 +0100

    remove a condition

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -26,7 +26,7 @@ int interpolation_search(int *array, size_t size, int value)
 		pos = low + (((double)(high - low) /
 				(array[high] - array[low])) * (value - array[low]));
 
-	if (pos >= size || pos < 0)
+	if (pos >= size)
 	{
 		printf("Value checked array[%lu] is out of range\n", pos);
 		return (-1);
